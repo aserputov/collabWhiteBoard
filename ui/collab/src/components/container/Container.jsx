ff6487ae1a06339007855efcfd740fdcffc8e3ce
Anatoliy Serputov <aserputov@myseneca.ca>
2022-02-21 12:12:29 -0500

    New React app created.

---
+++ ui/collab/src/components/container/Container.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Board from "../board/Board"
+class Container extends React.Component
+{
+    constructor(props)
+    {
+        super(props);
+    }
+
+    render()
+    {
+        return (
+            <div className="container">
+                <div className="color=picker">
+                    <input type="color"/>
+                </div>
+                <div class="board-container">
+                    <Board></Board>
+                </div>
+            </div>
+        )
+    }
+
+}
+export default Container
